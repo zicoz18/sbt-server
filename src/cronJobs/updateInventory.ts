b56c8d6a87a7b79d576872e98a0a9b4604fd41b8
zicoz18 <zicoz18@ku.edu.tr>
2021-08-09 22:26:49 +0300

    fixed typo

--- src/cronJobs/updateInventory.ts
+++ src/cronJobs/updateInventory.ts
@@ -1,36 +1,18 @@
-import {inject, service} from '@loopback/core';
+import {service} from '@loopback/core';
 import {CronJob, cronJob} from '@loopback/cron';
-import {repository} from '@loopback/repository';
-import {InventoryRepository, InventoryWithDateRepository, ItemNameRepository, ItemPriceWithDateRepository} from '../repositories';
-import {SteamService, UpdateInventoryService} from '../services';
+import {UpdateInventoryService} from '../services';
 
 @cronJob()
 export class UpdateInventory extends CronJob {
   constructor(
-    @inject('services.Steam')
-    protected steamService: SteamService,
-
-    @repository(InventoryRepository)
-    public inventoryRepository: InventoryRepository,
-
-    @repository(ItemNameRepository)
-    public itemNameRepository: ItemNameRepository,
-
-    @repository(InventoryWithDateRepository)
-    public inventoryWithDateRepository: InventoryWithDateRepository,
-
-    @repository(ItemPriceWithDateRepository)
-    public itemPriceWithDateRepository: ItemPriceWithDateRepository,
-
-    @service(UpdateInventory)
+    @service(UpdateInventoryService)
     protected updateInventoryService: UpdateInventoryService,
-
   ) {
     super({
       name: 'update-inventory',
       onTick: async () => {
         // do the work
-        this.performMyJob();
+        this.updateInventoryService.update();
       },
       // cronTime: '*/10 * * * * *', // Every ten second
       // cronTime: '0 12 * * * *', // Everyday at 12.00 (sanırım her saatin 12. dakikasinda update'liyor şuanda)
@@ -39,54 +21,5 @@ export class UpdateInventory extends CronJob {
       cronTime: '5 11 * * *', // everday at 11.05
       start: true,
     });
-  }
-
-  async performMyJob() {
-    await this.updateInventoryService.update();
-    // const currentDate = new Date();
-    // const inventory = await this.inventoryRepository.findOne({
-    //   where: {name: 'ziya'},
-    //   include: [{
-    //     relation: 'inventoryItems',
-    //     scope: {
-    //       include: ['itemName'],
-    //     },
-    //   }]
-    // });
-    // if (inventory?.inventoryItems) {
-    //   const itemPricesWithDate: ItemPriceWithDate[] = [];
-    //   let inventoryBalance = 0;
-
-    //   for (const inventoryItem of inventory.inventoryItems) {
-    //     try {
-    //       const steamItemInfo = await this.steamService.getItemInfo((<InventoryItem & InventoryItemRelations>inventoryItem).itemName.market_hash_name);
-    //       const itemPriceWithDate = <ItemPriceWithDate>{
-    //         price: steamItemInfo.median_price,
-    //         date: currentDate,
-    //         itemNameId: inventoryItem.itemNameId
-    //       };
-    //       itemPricesWithDate.push(itemPriceWithDate);
-    //       inventoryBalance += inventoryItem.amount * parseFloat(itemPriceWithDate.price.replace(",", "."));
-    //     } catch (error) {
-    //       throw new HttpErrors[422]('Cannot get item info from steam api.');
-    //     }
-    //   }
-    //   const inventoryWithDate = <InventoryWithDate>{
-    //     date: currentDate,
-    //     balance: inventoryBalance,
-    //     inventoryId: inventory.id
-    //   };
-    //   try {
-    //     const createdItemPricesWithDate = await this.itemPriceWithDateRepository.createAll(itemPricesWithDate);
-    //     const createdInventoryWithDate = await this.inventoryWithDateRepository.create(inventoryWithDate);
-    //     // console.log(`Successfully update at ${currentDate}\n\nCreated ItemPricesWithDate: \n${createdItemPricesWithDate} \n\nCreatedInventoryWithDate: \n${createdInventoryWithDate}\n\n`);
-    //     console.log(`Database Updated at ${currentDate}`);
-    //   } catch (error) {
-    //     // throw new HttpErrors[422]('Cannot create instances.');
-    //     console.log(error)
-    //   }
-    // } else {
-    //   throw new HttpErrors[422]('Inventory\'s items cannot be found.');
-    // }
   }
 }
