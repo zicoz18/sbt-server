e3cbf757e74a461d74bef73c88735754d3f60e9a
zicoz18 <zicoz18@ku.edu.tr>
2021-08-09 22:09:42 +0300

    added telegram bot

--- src/cronJobs/updateInventory.ts
+++ src/cronJobs/updateInventory.ts
@@ -1,10 +1,8 @@
-import {inject} from '@loopback/core';
+import {inject, service} from '@loopback/core';
 import {CronJob, cronJob} from '@loopback/cron';
 import {repository} from '@loopback/repository';
-import {HttpErrors} from '@loopback/rest';
-import {InventoryItem, InventoryItemRelations, InventoryWithDate, ItemPriceWithDate} from '../models';
 import {InventoryRepository, InventoryWithDateRepository, ItemNameRepository, ItemPriceWithDateRepository} from '../repositories';
-import {SteamService} from '../services';
+import {SteamService, UpdateInventoryService} from '../services';
 
 @cronJob()
 export class UpdateInventory extends CronJob {
@@ -23,6 +21,10 @@ export class UpdateInventory extends CronJob {
 
     @repository(ItemPriceWithDateRepository)
     public itemPriceWithDateRepository: ItemPriceWithDateRepository,
+
+    @service(UpdateInventory)
+    protected updateInventoryService: UpdateInventoryService,
+
   ) {
     super({
       name: 'update-inventory',
@@ -40,50 +42,51 @@ export class UpdateInventory extends CronJob {
   }
 
   async performMyJob() {
-    const currentDate = new Date();
-    const inventory = await this.inventoryRepository.findOne({
-      where: {name: 'ziya'},
-      include: [{
-        relation: 'inventoryItems',
-        scope: {
-          include: ['itemName'],
-        },
-      }]
-    });
-    if (inventory?.inventoryItems) {
-      const itemPricesWithDate: ItemPriceWithDate[] = [];
-      let inventoryBalance = 0;
+    await this.updateInventoryService.update();
+    // const currentDate = new Date();
+    // const inventory = await this.inventoryRepository.findOne({
+    //   where: {name: 'ziya'},
+    //   include: [{
+    //     relation: 'inventoryItems',
+    //     scope: {
+    //       include: ['itemName'],
+    //     },
+    //   }]
+    // });
+    // if (inventory?.inventoryItems) {
+    //   const itemPricesWithDate: ItemPriceWithDate[] = [];
+    //   let inventoryBalance = 0;
 
-      for (const inventoryItem of inventory.inventoryItems) {
-        try {
-          const steamItemInfo = await this.steamService.getItemInfo((<InventoryItem & InventoryItemRelations>inventoryItem).itemName.market_hash_name);
-          const itemPriceWithDate = <ItemPriceWithDate>{
-            price: steamItemInfo.median_price,
-            date: currentDate,
-            itemNameId: inventoryItem.itemNameId
-          };
-          itemPricesWithDate.push(itemPriceWithDate);
-          inventoryBalance += inventoryItem.amount * parseFloat(itemPriceWithDate.price.replace(",", "."));
-        } catch (error) {
-          throw new HttpErrors[422]('Cannot get item info from steam api.');
-        }
-      }
-      const inventoryWithDate = <InventoryWithDate>{
-        date: currentDate,
-        balance: inventoryBalance,
-        inventoryId: inventory.id
-      };
-      try {
-        const createdItemPricesWithDate = await this.itemPriceWithDateRepository.createAll(itemPricesWithDate);
-        const createdInventoryWithDate = await this.inventoryWithDateRepository.create(inventoryWithDate);
-        // console.log(`Successfully update at ${currentDate}\n\nCreated ItemPricesWithDate: \n${createdItemPricesWithDate} \n\nCreatedInventoryWithDate: \n${createdInventoryWithDate}\n\n`);
-        console.log(`Database Updated at ${currentDate}`);
-      } catch (error) {
-        // throw new HttpErrors[422]('Cannot create instances.');
-        console.log(error)
-      }
-    } else {
-      throw new HttpErrors[422]('Inventory\'s items cannot be found.');
-    }
+    //   for (const inventoryItem of inventory.inventoryItems) {
+    //     try {
+    //       const steamItemInfo = await this.steamService.getItemInfo((<InventoryItem & InventoryItemRelations>inventoryItem).itemName.market_hash_name);
+    //       const itemPriceWithDate = <ItemPriceWithDate>{
+    //         price: steamItemInfo.median_price,
+    //         date: currentDate,
+    //         itemNameId: inventoryItem.itemNameId
+    //       };
+    //       itemPricesWithDate.push(itemPriceWithDate);
+    //       inventoryBalance += inventoryItem.amount * parseFloat(itemPriceWithDate.price.replace(",", "."));
+    //     } catch (error) {
+    //       throw new HttpErrors[422]('Cannot get item info from steam api.');
+    //     }
+    //   }
+    //   const inventoryWithDate = <InventoryWithDate>{
+    //     date: currentDate,
+    //     balance: inventoryBalance,
+    //     inventoryId: inventory.id
+    //   };
+    //   try {
+    //     const createdItemPricesWithDate = await this.itemPriceWithDateRepository.createAll(itemPricesWithDate);
+    //     const createdInventoryWithDate = await this.inventoryWithDateRepository.create(inventoryWithDate);
+    //     // console.log(`Successfully update at ${currentDate}\n\nCreated ItemPricesWithDate: \n${createdItemPricesWithDate} \n\nCreatedInventoryWithDate: \n${createdInventoryWithDate}\n\n`);
+    //     console.log(`Database Updated at ${currentDate}`);
+    //   } catch (error) {
+    //     // throw new HttpErrors[422]('Cannot create instances.');
+    //     console.log(error)
+    //   }
+    // } else {
+    //   throw new HttpErrors[422]('Inventory\'s items cannot be found.');
+    // }
   }
 }
